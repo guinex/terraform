512bd0901efe4fbb2a6dcf6af080b681bd7e2032
Saurabh Chavhan <saurabh.chavhan@outlook.com>
2025-09-01 14:48:27 -0500

    feat: postgres cluster files updated

--- infra-setup/postgres-cluster/main.tf
+++ infra-setup/postgres-cluster/main.tf
@@ -17,6 +17,33 @@ resource "null_resource" "set_kubeconfig" {
   depends_on = [null_resource.install_k3s]
 }
 
+# Persistent Volume using NFS
+resource "kubernetes_persistent_volume" "postgres_pv" {
+  metadata {
+    name = "postgres-nfs-pv"
+  }
+  spec {
+    capacity = {
+      storage = "10Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    persistent_volume_reclaim_policy = "Retain" # 🔑 keeps data safe during Helm uninstall
+    storage_class_name = "postgres-nfs-storage"
+    nfs {
+      path   = "/mnt/nfs/postgres-data" # ✅ Your NFS mount point
+      server = "YOUR_NFS_SERVER_IP"     # 👈 replace with actual NFS server IP
+    }
+  }
+}
+
+resource "kubernetes_storage_class" "postgres_sc" {
+  metadata {
+    name = "postgres-nfs-storage"
+  }
+  storage_provisioner    = "kubernetes.io/no-provisioner"
+  volume_binding_mode    = "WaitForFirstConsumer"
+}
+
 resource "helm_release" "postgres" {
   name             = "postgres"
   namespace        = "database"
@@ -50,8 +77,37 @@ resource "helm_release" "postgres" {
     value = "31432"
   }
 
+  # ✅ This makes Postgres use your NFS folder for persistence
   set {
     name  = "primary.persistence.enabled"
     value = "true"
+  }
+
+  set {
+    name  = "primary.persistence.storageClass"
+    value = kubernetes_storage_class.postgres_sc.metadata[0].name
+  }
+
+  set {
+    name  = "primary.persistence.existingClaim"
+    value = "postgres-pvc"
+  }
+}
+
+# Persistent Volume Claim to bind PV -> Postgres Helm chart
+resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+  metadata {
+    name      = "postgres-pvc"
+    namespace = "database"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+    storage_class_name = kubernetes_storage_class.postgres_sc.metadata[0].name
+    volume_name        = kubernetes_persistent_volume.postgres_pv.metadata[0].name
   }
 }
